a52cd6bbcab7dc09d3872bf9da64f34724166ab5
BB <bram.nicaise.1@gmail.com>
2017-06-25 19:06:20 +0200

    Added more variants

---
+++ B_NMBS/TS/RailNetwork/Signals/BE Colour Light/BE 3AspSig Post Auto.lua
@@ -0,0 +1,19 @@
+--------------------------------------------------------------------------------------
+-- UK 3 Aspect Signal Post
+-- KUJU / Rail Simulator
+--------------------------------------------------------------------------------------
+
+--include=Common BE 3AspSig.lua
+--include=CommonScripts\Common BE Colour Light Script.lua
+--include=..\CommonScripts\New Common Signal Script.lua
+ 
+--------------------------------------------------------------------------------------
+-- INITIALISE
+--
+function Initialise ()
+
+	-- This is a post signal, so need reference to the attached signal head to switch lights on and off
+	SIGNAL_HEAD_NAME = "3 Aspect Signal Head:"
+	
+	DefaultInitialise( SIGNAL_TYPE_AUTO )
+end
